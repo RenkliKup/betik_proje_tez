aa61f84b72b105a9d28bc55a62633c3cac68a8c4
Renklikup <baris.1907.krklk@gmail.com>
2022-01-02 20:33:27 +0300

    initial_commit

--- blink_detect.py
+++ blink_detect.py
@@ -1,7 +1,8 @@
 #All the imports go here
 import numpy as np
 import cv2
-
+import time
+import subprocess
 #Initializing the face and eye cascade classifiers from xml files
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
@@ -12,6 +13,8 @@ first_read = True
 #Starting the video capture
 cap = cv2.VideoCapture(0)
 ret,img = cap.read()
+eyes_t=time.time()
+face_t=time.time()
 
 while(ret):
     ret,img = cap.read()
@@ -21,44 +24,52 @@ while(ret):
     gray = cv2.bilateralFilter(gray,5,1,1)
 
     #Detecting the face for region of image to be fed to eye classifier
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5,minSize=(200,200))
+    faces = face_cascade.detectMultiScale(gray, 1.3, 3,minSize=(200,200))
+    
     if(len(faces)>0):
+        face_t=time.time()
+        
         for (x,y,w,h) in faces:
             img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
             #roi_face is face which is input to eye classifier
             roi_face = gray[y:y+h,x:x+w]
             roi_face_clr = img[y:y+h,x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_face,1.3,5,minSize=(50,50))
+            eyes = eye_cascade.detectMultiScale(roi_face,1.3,3,minSize=(50,50))
 
             #Examining the length of eyes object for eyes
             if(len(eyes)>=2):
+                eyes_t=time.time()
                 #Check if program is running for detection 
                 if(first_read):
                     cv2.putText(img, "Eye detected press s to begin", (70,70), cv2.FONT_HERSHEY_PLAIN, 3,(0,255,0),2)
                 else:
                     cv2.putText(img, "Eyes open!", (70,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,255,255),2)
             else:
-                if(first_read):
-                    #To ensure if the eyes are present before starting
-                    cv2.putText(img, "No eyes detected", (70,70), cv2.FONT_HERSHEY_PLAIN, 3,(0,0,255),2)
-                else:
-                    #This will print on console and restart the algorithm
-                    print("Blink detected--------------")
-                    cv2.waitKey(3000)
-                    first_read=True
+                eyes_end_time=time.time()
+                eyes_two_minutes=eyes_end_time-eyes_t
+                cv2.putText(img, f"Eyes close in {eyes_two_minutes}", (70,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,255,255),2)
+                
+                if(eyes_two_minutes>60):
+                    print("göz için kapatttt")
+                
+               
             
     else:
-        cv2.putText(img,"No face detected",(100,100),cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0),2)
+            face_end_time=time.time()
+            face_two_minutes=face_end_time-face_t
+            cv2.putText(img, f"Eyes close in {face_two_minutes}", (70,70), cv2.FONT_HERSHEY_PLAIN, 2,(255,255,255),2)
+            
+            if(face_two_minutes>60):
+                print("yüz için kapatttt")
+        
 
     #Controlling the algorithm with keys
     cv2.imshow('img',img)
     a = cv2.waitKey(1)
     if(a==ord('q')):
         break
-    elif(a==ord('s') and first_read):
-        #This will start the detection
-        first_read = False
+    
 
 cap.release()
 cv2.destroyAllWindows()
